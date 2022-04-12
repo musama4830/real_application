56698cf9f7b280aabf4f8d8f242a87ca655591c3
musama4830 <musama4830@gmail.com>
2022-04-12 13:52:40 +0500

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Pages/my_app.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
+}
